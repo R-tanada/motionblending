3d7b884f882dd8ae18dd8373c1a2d7eb54494154
R-tanada <r.tanada.550@nitech.jp>
2023-05-17 15:13:30 +0900

    fix

--- RobotArmControl/SafetyManager.py
+++ RobotArmControl/SafetyManager.py
@@ -8,7 +8,7 @@ class SafetyManager:
         self.Init_X, self.Init_Y, self.Init_Z, self.Init_Roll, self.Init_Pitch, self.Init_Yow = self.SetInitial(xArmConfigs['InitPos'], xArmConfigs['InitRot'])
         self.Max_X, self.Max_Y, self.Max_Z, self.Max_Roll, self.Max_Pitch, self.Max_Yow = self.SetInitial(xArmConfigs['MaxPos'], xArmConfigs['MaxRot'])
         self.Min_X, self.Min_Y, self.Min_Z, self.Min_Roll, self.Min_Pitch, self.Min_Yow = self.SetInitial(xArmConfigs['MinPos'], xArmConfigs['MinRot'])
-        self.initRot = cf.Euler2Quaternion(xArmConfigs['InitRot'])
+        self.initRot = cf.Convert2Matrix_Quaternion(cf.Euler2Quaternion(xArmConfigs['InitRot']))
 
     def SetInitial(self, InitPos, InitRot):
         return InitPos[0], InitPos[1], InitPos[2], InitRot[0], InitRot[1], InitRot[2]
@@ -20,7 +20,7 @@ class SafetyManager:
         return MinPos[0], MinPos[1], MinPos[2], MinRot[0], MinRot[1], MinRot[2]
 
     def CheckLimit(self, position, rotation):
-        rotation = cf.Quaternion2Euler(np.dot(cf.Convert2Matrix_Quaternion(self.initRot), rotation))
+        rotation = cf.Quaternion2Euler(np.dot(self.initRot, rotation))
         x, y, z = position[0] + self.Init_X, position[1] + self.Init_Y, position[2] + self.Init_Z
         roll, pitch, yow = rotation[0], rotation[1], rotation[2]
 
