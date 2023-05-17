db17c5abda53c040ef53e4bffc90e7013f61d29a
R-tanada <r.tanada.550@nitech.jp>
2023-05-17 16:01:22 +0900

    fix

--- test.py
+++ test.py
@@ -1,9 +1,3 @@
-import CustomFunction.Calculation as cf
-import numpy as np
-
-q_init = [0, 0, 0, 1]
-q_1 = [0.01, 0.01, 0.01, 0.97]
-
-matrix = cf.Convert2Matrix_Quaternion(q_1)
-q_2 = np.dot(matrix, q_init)
-print(q_2)
+a = {'a': 1, 'b': 2}
+b = a['a']
+print(a)
