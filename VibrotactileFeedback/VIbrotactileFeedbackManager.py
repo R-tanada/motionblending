ac6ef297baa4de7d08423bbb2c24dfa6616c9879
R-tanada <r.tanada.550@nitech.jp>
2023-05-18 19:00:17 +0900

    fix

--- VibrotactileFeedback/VIbrotactileFeedbackManager.py
+++ VibrotactileFeedback/VIbrotactileFeedbackManager.py
@@ -1,22 +1,29 @@
 import pyaudio
+import numpy as np
+import time
 
 class VibrotactileFeedbackManager:
     def __init__(self) -> None:
-        p = pyaudio()
+        p = pyaudio.PyAudio()
         stream = self.OpenStream(p)
         stream.start_stream()
+        self.sin = np.sin(2.0 * np.pi * np.arange(int(44100/200)) * float(200) / float(44100))
 
     def OpenStream(self, p):
         stream = p.open(
-            format = 1, 
-            channnels = 1, 
-            rate = 1, 
+            format = pyaudio.paInt16, 
+            channels = 1, 
+            rate = 44100, 
             output = True,
-            frames_per_buffer = 1, 
+            frames_per_buffer = int(44100/200), 
             output_device_index = 1,
-            stream_callback = self.callback
+            stream_callback = self.Callback
         )
 
         return stream
-        
-
+    
+    def Callback(self, in_data, frame_count, time_info, status):
+        out_data = (self.sin).astype(np.int16)
+        return (out_data, pyaudio.paContinue)
+    
+    
