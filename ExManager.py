714e7ebb78c6653ea625cd6ea0a243d81ae28789
R-tanada <r.tanada.550@nitech.jp>
2023-05-18 17:01:57 +0900

    fix

--- ExManager.py
+++ ExManager.py
@@ -5,6 +5,6 @@ from RobotArmControl.RobotControlManager import RobotControlManager
 
 if __name__ == '__main__':
     robotControlManager = RobotControlManager()
-    robotControlManager.SendDataToRobot(isEnablexArm = True, FrameRate = 240, isPrintFrameRate = True)
+    robotControlManager.SendDataToRobot(isEnablexArm = True, FrameRate = 240, isPrintFrameRate = False)
 
     print('\n----- End program: ExManager.py -----')
