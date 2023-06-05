b9957573582483c5f541ee2ad00f8380705e6de4
R-tanada <r.tanada.550@nitech.jp>
2023-06-05 14:45:25 +0900

    fix

--- iter_test.py
+++ iter_test.py
@@ -1,8 +1,21 @@
-from itertools import cycle as iter_cycle
+import threading
+import time
 
-a = [{'position': [0, 0, 0], 'rotation': [0, 0, 0]}, {'position': [1, 1, 1], 'rotation': [1, 1, 1]}]
+def test1():
+    while True:
+        print('test1')
+        time.sleep(0.5)
 
-a_iter = iter_cycle(a)
+def test2():
+    test1_thread = threading.Thread(target=test1)
+    test1_thread.setDaemon(True)
+    test1_thread.start()
+    print('hello')
 
-while True:
-    print(next(a_iter)['position'])
+    while True:
+        print('test2')
+        time.sleep(0.5)
+
+test2_thread = threading.Thread(target=test2)
+test2_thread.setDaemon(True)
+test2_thread.start()
