84dde68e1215c756f6ab0cb508155b84482dcc93
R-tanada <r.tanada.550@nitech.jp>
2023-06-21 00:09:30 +0900

    fix

--- test.py
+++ test.py
@@ -1,10 +1,47 @@
-t3 = 2.921556416
-t0 = 2.634491127545451
-tf = 0.3844867703584493
-x3 = 450
-xf = 500
+from subprocess import Popen, PIPE, STDOUT
+import signal
+import tkinter as tk
 
-t = (t3 - t0)/ tf
-print(t)
-s = 6*(t**5) - 15*(t**4) + 10*(t**3)
-print((x3 - xf*s)/ (1 - s))
+cmd = ('python ExManager.py').split()
+
+def killProcess():
+    if 'proc' in globals():
+        proc.terminate()
+        proc.wait()
+        output, err = proc.communicate()
+        output = output.decode('shift_jis')
+        print(output)
+    root.destroy()
+
+def handler(signum, root):
+    print("handler")
+    killProcess()
+
+def ctrlC(event):
+    print("ctrlC")
+    killProcess()
+
+def stop():
+    print("stop")
+    killProcess()
+
+def start():
+    global proc
+    print("start")
+    proc = Popen(cmd, stdout=PIPE, stderr=STDOUT)
+
+def check(): 
+    root.after(500, check)  #  time in ms.
+#
+signal.signal(signal.SIGINT, handler) # ctrl-c from console
+
+root = tk.Tk()
+root.after(500, check)  #  time in ms.
+
+tk.Button(root, text="Start subprocess", command=start).pack()
+tk.Button(root, text="Stop subprocess", command=stop).pack()
+root.bind('<Control-c>', ctrlC) 
+root.protocol("WM_DELETE_WINDOW", stop)
+
+root.mainloop()
+print("exited")
