76a02145a5984ddde1d50dde4a4a9fb31a59478a
R-tanada <r.tanada.550@nitech.jp>
2023-09-25 17:43:29 +0900

    fix

--- ExManager.py
+++ ExManager.py
@@ -1,4 +1,4 @@
-from CyberneticAvatarMotionManager import CyberneticAvatarMotionManager
+from motioncontrol import CyberneticAvatarMotionManager
 
 if __name__ == '__main__':
     CyberneticAvatarMotionManager(is_Simulation = False, is_Visualize = False, is_Recording = False, is_Plotting = True)
