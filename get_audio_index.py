087fcc592011c0c71e76f17306ed7d83166ef1ec
R-tanada <r.tanada.550@nitech.jp>
2023-11-14 14:56:05 +0900

    fix

--- get_audio_index.py
+++ get_audio_index.py
@@ -2,4 +2,6 @@ import pyaudio
 
 iaudio = pyaudio.PyAudio()
 for i in range(0, iaudio.get_device_count()):
-    print(iaudio.get_device_info_by_index(i))
+    index = iaudio.get_device_info_by_index(i)
+    if index['maxInputChannels'] == 0:
+        print(index)
