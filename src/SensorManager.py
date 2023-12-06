50068f65c0d27e6e1f4a500f7ddd59ffe43d408a
R-tanada <r.tanada.550@nitech.jp>
2023-12-06 14:41:37 +0900

    fix

--- src/SensorManager.py
+++ src/SensorManager.py
@@ -27,7 +27,7 @@ class GripperSensorManager:
                 else:
                     self.sensorValue = 850
 
-                time.sleep(0.005)
+                time.sleep(0.004)
 
         except KeyboardInterrupt:
             print("KeyboardInterrupt >> Stop: BendingSensorManager.py")
@@ -50,7 +50,7 @@ class FootSwitchManager:
                     FootSwitchManager.flag = True
                     print("----- foot switch pressed -----")
 
-                time.sleep(0.005)
+                time.sleep(0.004)
 
         except:
             print("error occured")
