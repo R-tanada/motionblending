4ea4393d4317d4cba0c4661ca63944c6278fba6b
takumi nishimura <112153843+Haptics-Lab@users.noreply.github.com>
2023-12-07 18:43:30 +0900

    fix

---
+++ random_condition.py
@@ -0,0 +1,5 @@
+import random
+
+print(random.sample(range(1, 5), k = 4))
+
+
--- src/mode_select.py
+++ src/mode_select.py
@@ -1,4 +1,4 @@
-mode = 4
-name = "kurimoto"
+mode = 3
+name = "tanada"
 path_right = "resource\SI2023\sibata20231017_175442.csv"
 path_left = "resource\SI2023\kaiyu20231018_134750.csv"
