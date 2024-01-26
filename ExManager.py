260d4736e79e351d8841157fa6d9d84cc70fb7e2
takumi nishimura <112153843+Haptics-Lab@users.noreply.github.com>
2024-01-26 13:48:19 +0900

    fix

--- ExManager.py
+++ ExManager.py
@@ -18,10 +18,11 @@ class ExManager:
         self.is_Plotting = is_Plotting
         self.is_Recording = is_Recording
 
-        with open("docs/settings_singlel.json", "r") as settings_file:
+        with open("docs/settings_single.json", "r") as settings_file:
             settings = json.load(settings_file)
 
-        from src.CyberneticAvatarMotionManager import CyberneticAvatarMotionManager
+        from src.CyberneticAvatarMotionManager import \
+            CyberneticAvatarMotionManager
 
         self.cyberneticManager = CyberneticAvatarMotionManager(
             settings["ParticipantsConfigs"],
--- src/mode_select.py
+++ src/mode_select.py
@@ -1,5 +1,5 @@
 # 0:model_record, 1:without assist, 2:personalize, 3:vibrotactile, 4:LED_feedback, 5:free
-mode = 2
+mode = 5
 name = "iida"
 path_right = "resource\iida\model_data/right20240122_090516.csv"
 path_left = "resource\iida\model_data\left20240122_090519.csv"
